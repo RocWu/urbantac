9257e2c750ad0fd05e69e5d5da50aef103573b22
Michael Varrieur <mike.varrieur@gmail.com>
2013-04-09 20:55:26 -0400

    Missed some files

---
+++ wp-content/themes/urbantac/products-page.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Products Page
+*/
+?>
+
+<?php get_header(); ?>
+
+  <div id="content-wrap">
+    <div id="products-content" class="products wrap clearfix aligncenter content-container">
+      THIS IS A DUMMY PAGE. SEE archive-product.php for the actual code.
+    </div> <!-- end #products-content -->
+  </div> <!-- end #about-wrap -->
+<?php get_footer(); ?>
